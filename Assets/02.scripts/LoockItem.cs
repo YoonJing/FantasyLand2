294a456074f8e13aaeb0f2875a665ca350549ff0
YoonJing <66227808+YoonJing@users.noreply.github.com>
2020-11-03 15:30:38 +0900

    11-3

---
+++ Assets/02.scripts/LoockItem.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class LoockItem : MonoBehaviour, IPointerEnterHandler,
+    IPointerExitHandler, IGvrPointerHoverHandler
+{
+    public void OnGvrPointerHover(PointerEventData eventData)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnLookItemBox(bool isLookAt)
+    {
+        Debug.Log(isLookAt);
+        MoveCtrl.isStopped = isLookAt;
+    }
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        MoveCtrl.isStopped = true;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        MoveCtrl.isStopped = true;
+    }
+    public void OnPointerHover(PointerEventData eventData)
+    {
+        Debug.Log("Retic On");
+    }
+
+}
